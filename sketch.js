70643c40ff928322f12a92c15f55a9b7ebf09ce1
bnray53 <bnray53@gmail.com>
2019-07-17 15:23:04 -0500

    Optimized version of Conways Game

--- sketch.js
+++ sketch.js
@@ -1,22 +1,11 @@
 //Resolution is the side length of the grid squares, any grid variables are responsive to resolution 
-var resolution = 14;
-
-/*What state the swap button is on, default is 0 "Random Grid" with solid grid displayed,
-1 is "Solid Grid" with random grid displayed*/
-var swapButtonState=1;
+var resolution = 8;
 
 //Declaring variables
 var gridArray;
 var gridXSize;
 var gridYSize;
 
-/*Class for objects residing in the 2-dimensional array, only attribute they have is .ind which acts as 
-an on/off or 1/0 indicator of the grid's state*/
-class GridContent {
-    constructor(ind) {
-        this.ind = ind;
-    }
-}
 
 function setup() {
     /*NOTE: canvas dimensions should be able to be divided evenly by resolution if you want grid squares that fill
@@ -36,14 +25,15 @@ function setup() {
 
     //Creating two-dimensional array that will be represented by a grid of clickable squares
     gridArray = [];
+
+    frameRate(5);
+
     //Drawing initial grid
     createGrid();
-    draw();
 }
 
 function draw() {
-    background(255);
-    generate();
+    createNextGeneration();
     drawGrid();
 }
 
@@ -53,64 +43,31 @@ function windowResized(){
     setup();
 }
 
-
-
-/*NOTE: createGrid() function is only called during setup() function and swapGrid() function,
- fillGrid() function is only called during createGrid() function*/
-
-
 function createGrid() {
-    
-    for (i = 0; i < gridXSize; i++) {
-        gridArray[i] = [];
-        for (j = 0; j < gridYSize; j++) {
-            fillGrid(i, j);
+    for (x = 0; x < gridXSize; x++) {
+        gridArray[x] = [];
+        for (y = 0; y < gridYSize; y++) {
+            //fillGrid(x, y);
+            gridArray[x][y]=(floor(random(0, 2)));
         }
     }
 }
 
-/*Creating white or random grid squares with size being respect to pixels and creating "state" object 
-in each cell that is initially set to 1 for white, and 1 or 0 for random. 
-grid being solid or random is subject to swapButtonState*/
-function fillGrid(x, y) {
-    var fx=x;
-    var fy=y;
-    var indicator;
-    //Drawing solid grid
-    if(swapButtonState==0){
-        fill("white");
-        rect((fx * resolution), (fy * resolution), resolution, resolution);
-        indicator = 1;
-        gridArray[fx][fy] = new GridContent(indicator);
-    }
-    //Drawing random grid
-    else if(swapButtonState==1){
-        var rand = floor(random(0, 2));
-        if (rand == 1) {
-            fill("white");
-            rect((fx * resolution), (fy * resolution), resolution, resolution);
-            indicator = 0;
-            gridArray[fx][fy] = new GridContent(indicator);
-        } else {
-            fill("black");
-            rect((fx * resolution), (fy * resolution), resolution, resolution);
-            indicator = 1;
-            gridArray[fx][fy] = new GridContent(indicator);
+function createArray(){
+    var array=[];
+    for(i=0;i<gridXSize;i++){
+        array[i]=[];
+        for(j=0;j<gridYSize;j++){
+            array[i][j]=0;
         }
     }
-    return;
+    return array;
 }
 
-function generate(){
-    var newArray=[];
-    for (m = 0; m < gridXSize; m++) {
-        newArray[m]=[];
-        for (n = 0; n < gridYSize; n++) {
-            var indicator=0;
-            newArray[m][n] = new GridContent(indicator);
-        }
-    }
-    
+function createNextGeneration(){
+
+    var newArray=createArray();
+
     for (i = 1; i < gridXSize-1; i++) {
         for (j = 1; j < gridYSize-1; j++) {
 
@@ -118,33 +75,31 @@ function generate(){
 
             for (k = -1; k <= 1; k++) {
                 for (l = -1; l <= 1; l++) {
-                    neighbors += gridArray[i+k][j+l].ind;
+                    neighbors += gridArray[i+k][j+l];
                 }
             }
 
-            neighbors -= gridArray[i][j].ind;
-            console.log('here');
+            neighbors -= gridArray[i][j];
+            
             //Rules
-            if((gridArray[i][j].ind==1) && (neighbors<2)){
-                newArray[i][j].ind=0;
-            }else if((gridArray[i][j].ind==1) && (neighbors>3)){
-                newArray[i][j].ind=0;
-            }else if((gridArray[i][j].ind==0) && (neighbors==3)){
-                newArray[i][j].ind=1;
+            if((gridArray[i][j]==1) && (neighbors<2)){
+                newArray[i][j]=0;
+            }else if((gridArray[i][j]==1) && (neighbors>3)){
+                newArray[i][j]=0;
+            }else if((gridArray[i][j]==0) && (neighbors==3)){
+                newArray[i][j]=1;
             }else{
-                newArray[i][j].ind=gridArray[i][j].ind;                
+                newArray[i][j]=gridArray[i][j];                
             }
         }
     }
-
     gridArray=newArray;
-
 }
 
 function drawGrid(){
     for(i=0;i<gridXSize;i++){
         for(j=0;j<gridYSize;j++){
-            if(gridArray[i][j].ind == 1){
+            if(gridArray[i][j] == 1){
                 fill("black");
                 rect((i * resolution), (j * resolution), resolution, resolution);
             }else{
